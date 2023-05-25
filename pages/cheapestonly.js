17cfe2ff1aaebd92f517a8f751a656eaf5154d73
alexrubens <rubensjalex@gmail.com>
2023-05-25 13:10:58 -0700

    2 files modified, 1 added

--- pages/cheapestonly.js
+++ pages/cheapestonly.js
@@ -11,12 +11,15 @@ const cityNames = {
   JFK: 'New York, NY (JFK)',
 };
 
-function ResultsPage() {
+function CheapestOnlyPage() {
   const router = useRouter();
   const { location1, location2, destinations, departureDate, adults } =
     router.query;
 
   const [flights, setFlights] = useState({});
+  const [cheapestFlights, setCheapestFlights] = useState({});
+  const [searchProgress, setSearchProgress] = useState(0);
+  const [cheapestDestination, setCheapestDestination] = useState(null);
 
   useEffect(() => {
     if (location1 && location2 && destinations && departureDate && adults) {
@@ -39,6 +42,8 @@ function ResultsPage() {
           }
         );
         const { access_token } = await tokenResponse.json();
+
+        let completedSearches = 0;
 
         for (const destination of destinationList) {
           const response1 = await fetch(
@@ -73,9 +78,40 @@ function ResultsPage() {
               flight: data2.data[0],
             });
           }
+
+          completedSearches++;
+          setSearchProgress((completedSearches / destinationList.length) * 100);
         }
 
-        setFlights(flightData);
+        if (Object.keys(flightData).length) {
+          const cheapestFlightsData = {};
+
+          for (const [location, flightInfos] of Object.entries(flightData)) {
+            cheapestFlightsData[location] = flightInfos.reduce(
+              (prev, current) =>
+                prev.flight.price.total < current.flight.price.total
+                  ? prev
+                  : current
+            );
+          }
+
+          const combined = Object.values(cheapestFlightsData).reduce((a, b) => {
+            const totalA = parseFloat(a.flight.price.total);
+            const totalB = parseFloat(b.flight.price.total);
+            return totalA < totalB ? a : b;
+          });
+
+          const totalCost = Object.values(cheapestFlightsData).reduce(
+            (total, flightInfo) => {
+              return total + parseFloat(flightInfo.flight.price.total);
+            },
+            0
+          );
+
+          setCheapestDestination({ ...combined, totalCost });
+          setFlights(flightData);
+          setCheapestFlights(cheapestFlightsData);
+        }
       };
 
       fetchData();
@@ -84,33 +120,52 @@ function ResultsPage() {
 
   return (
     <div>
-      <h1>Results</h1>
-      {Object.entries(flights).map(([location, flightInfos]) => (
+      <h1>Cheapest Flights Only</h1>
+      <div>
+        <div
+          style={{
+            width: `${searchProgress}%`,
+            backgroundColor: 'green',
+            height: '20px',
+          }}
+        ></div>
+      </div>
+      <p>{`Progress: ${Math.round(searchProgress)}%`}</p>
+      {Object.entries(cheapestFlights).map(([location, flightInfo]) => (
         <div key={location}>
           <h2>Flights from {cityNames[location] || location}</h2>
-          {flightInfos.map((flightInfo, index) => (
-            <div key={index}>
-              <h3>
-                Destination:{' '}
-                {cityNames[flightInfo.destination] || flightInfo.destination}
-              </h3>
-              <p>Price: {flightInfo.flight.price.total}</p>
-              <h4>Flight Details:</h4>
-              <p>Duration: {flightInfo.flight.itineraries[0].duration}</p>
-              <p>
-                Number of Stops:{' '}
-                {flightInfo.flight.itineraries[0].segments.length - 1}
-              </p>
-              <p>
-                Airline:{' '}
-                {flightInfo.flight.itineraries[0].segments[0].carrierCode}
-              </p>
-            </div>
-          ))}
+          <h3>
+            Destination:{' '}
+            {cityNames[flightInfo.destination] || flightInfo.destination}
+          </h3>
+          <p>Price: {flightInfo.flight.price.total}</p>
+          <h4>Flight Details:</h4>
+          <p>Duration: {flightInfo.flight.itineraries[0].duration}</p>
+          <p>
+            Number of Stops:{' '}
+            {flightInfo.flight.itineraries[0].segments.length - 1}
+          </p>
+          <p>
+            Airline: {flightInfo.flight.itineraries[0].segments[0].carrierCode}
+          </p>
         </div>
       ))}
+      {cheapestDestination && (
+        <div>
+          <h2>
+            Cheapest Overall Destination:{' '}
+            {cityNames[cheapestDestination.destination] ||
+              cheapestDestination.destination}
+          </h2>
+          <h3>Total Cost: {cheapestDestination.totalCost.toFixed(2)}</h3>
+          <h3>
+            Average Cost (If Split Evenly):{' '}
+            {(cheapestDestination.totalCost / 2).toFixed(2)}
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
 
-export default ResultsPage;
+export default CheapestOnlyPage;
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,125 @@
-import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function Home() {
   const router = useRouter();
   const [location1, setLocation1] = useState('');
   const [location2, setLocation2] = useState('');
-  const [destinations, setDestinations] = useState('');
   const [departureDate, setDepartureDate] = useState('');
-  const [adults, setAdults] = useState('');
+  const [adults, setAdults] = useState(1);
+  const [checkboxDestinations, setCheckboxDestinations] = useState({
+    LAX: false,
+    SFO: false,
+    DFW: false,
+    SEA: false,
+    MIA: false,
+    LAS: false,
+    JFK: false,
+  });
+  const [additionalDestinations, setAdditionalDestinations] = useState('');
+  const [cheapestOnly, setCheapestOnly] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    router.push(
-      `/results?location1=${location1}&location2=${location2}&destinations=${destinations}&departureDate=${departureDate}&adults=${adults}`
-    );
+    const selectedDestinations = Object.keys(checkboxDestinations)
+      .filter((destination) => checkboxDestinations[destination])
+      .concat(additionalDestinations.split(',').map((d) => d.trim()))
+      .join(',');
+
+    router.push({
+      pathname: cheapestOnly ? '/cheapestonly' : '/results',
+      query: {
+        location1,
+        location2,
+        destinations: selectedDestinations,
+        departureDate,
+        adults,
+      },
+    });
+  };
+
+  const handleCheckboxChange = (e) => {
+    setCheckboxDestinations({
+      ...checkboxDestinations,
+      [e.target.name]: e.target.checked,
+    });
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Location 1 (IATA Code):
         <input
+          type="text"
           value={location1}
           onChange={(e) => setLocation1(e.target.value)}
-          placeholder="Location 1"
+          required
         />
+      </label>
+      <br />
+      <label>
+        Location 2 (IATA Code):
         <input
+          type="text"
           value={location2}
           onChange={(e) => setLocation2(e.target.value)}
-          placeholder="Location 2"
+          required
         />
+      </label>
+      <br />
+      <label>
+        Destinations (comma-separated):
         <input
-          value={destinations}
-          onChange={(e) => setDestinations(e.target.value)}
-          placeholder="Destinations (comma-separated)"
+          type="text"
+          value={additionalDestinations}
+          onChange={(e) => setAdditionalDestinations(e.target.value)}
         />
+      </label>
+      <div>
+        {Object.keys(checkboxDestinations).map((destination) => (
+          <label key={destination}>
+            <input
+              type="checkbox"
+              name={destination}
+              checked={checkboxDestinations[destination]}
+              onChange={handleCheckboxChange}
+            />
+            {destination}
+          </label>
+        ))}
+      </div>
+      <br />
+      <label>
+        Departure Date:
         <input
           type="date"
           value={departureDate}
           onChange={(e) => setDepartureDate(e.target.value)}
-          placeholder="Departure Date"
+          required
         />
+      </label>
+      <br />
+      <label>
+        Number of Adults:
         <input
           type="number"
           value={adults}
           onChange={(e) => setAdults(e.target.value)}
-          placeholder="Adults"
           min="1"
+          required
         />
-        <button type="submit">Find Flights</button>
-      </form>
-    </div>
+      </label>
+      <br />
+      <label>
+        Cheapest Only:
+        <input
+          type="checkbox"
+          checked={cheapestOnly}
+          onChange={(e) => setCheapestOnly(e.target.checked)}
+        />
+      </label>
+      <br />
+      <button type="submit">Search</button>
+    </form>
   );
 }
